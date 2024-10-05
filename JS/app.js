4e6e0a58b62ff23cae30d90ec8c922b7a984acb0
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-10-06 03:13:21 +0600

    Folder structure designed

---
+++ JS/app.js
@@ -0,0 +1,32 @@
+// fetch("https://restcountries.com/v3.1/all")
+//   .then((res) => res.json())
+//   .then((countries) => {
+//     fetch("https://api.fxratesapi.com/latest")
+//       .then((res) => res.json())
+//       .then((curate) => {
+//         console.log(countries, "countries");
+//         console.log(curate, "curate");
+
+//         const curateNameArr = Object.keys(curate.rates);
+//         console.log(curateNameArr.length, "cur conv api rate no");
+//         const data = [];
+
+//         const newCoun = countries.filter((country) => country.currencies);
+
+//         newCoun.forEach((countryInfo) => {
+//           console.log(countryInfo.currencies);
+//           const curName = Object.keys(countryInfo.currencies)[0];
+
+//           if (curateNameArr.includes(curName)) {
+//             data.push({
+//               countryName: countryInfo.name.common,
+//               flagUrl: countryInfo.flags.png,
+//               currencyName: curName,
+//             });
+//           }
+//         });
+
+//         console.log(data, "new updated data");
+//         console.log(data.length, "new updated data");
+//       });
+//   });
