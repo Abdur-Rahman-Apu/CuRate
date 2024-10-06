fffa9036773d61f72048963d615dcb55e1329164
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-10-06 19:39:16 +0600

    Choose country section's country option part's country info reuseable function created for both base and target country

---
+++ JS/modules/ui/countryInfo/countryInfo.js
@@ -0,0 +1,19 @@
+export default function countryInfo({ type, flagUrl, countryName }) {
+  return type === "base"
+    ? `<div>
+      <img
+        src="${flagUrl}"
+        alt="This image is indicating the selected base currency's country flag image"
+        class="base-currency-flag-image"
+      />
+      <p class="base-currency-country-name">${countryName}</p>
+    </div>`
+    : `<div>
+      <img
+        src="${flagUrl}"
+        alt="This image is indicating the selected base currency's country flag image"
+        class="target-currency-flag-image"
+      />
+      <p class="target-currency-country-name">${countryName}</p>
+    </div>`;
+}
